e4ccd9853641c53d7a6ac6d3cc0cbd0792cc2785
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2013-07-26 10:00:21 +0900

    [Model/JapanesePref] Fix: options value

--- Model/JapanesePref.model.php
+++ Model/JapanesePref.model.php
@@ -73,12 +73,14 @@ class Model_JapanesePref extends Model_Model
 	
 	function UsedToForms( $type='object' )
 	{
+		if(!$type){ $type = 'object'; }
+		
 		//  base pref
-		$pref = self::Get();
+		$pref = $this->Get();
 		
 		//  init options
 		$options = new Config();
-		$options->{0}->value = '';
+		$options->{'00'}->value = '';
 		
 		foreach( $pref as $value => $label ){
 			if($type == 'object'){
