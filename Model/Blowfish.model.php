fcf7fa891c7b423453fa93793c4d03088824ec94
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2013-11-14 18:33:47 +0900

    [Model/Blowfish] Change: optimized

--- Model/Blowfish.model.php
+++ Model/Blowfish.model.php
@@ -1,25 +1,33 @@
 <?php
 
-class Model_Blowfish extends Model_Model
+class Model_Blowfish /* extends Model_Model */
 {
 	/**
 	 * @var Blowfish
 	 */
 	private $_blowfish = null;
 	
+	/*
 	function Init()
 	{
 		parent::Init();
 		$this->_blowfish = new Blowfish();
 	}
+	*/
 	
 	function Encrypt( $data, $password=null )
 	{
+		if(!$this->_blowfish){
+			$this->_blowfish = new Blowfish();
+		}
 		return $this->_blowfish->Encrypt( $data, $password );
 	}
 	
 	function Decrypt( $string, $password=null )
 	{
+		if(!$this->_blowfish){
+			$this->_blowfish = new Blowfish();
+		}
 		return $this->_blowfish->Decrypt( $string, $password );
 	}
 }
