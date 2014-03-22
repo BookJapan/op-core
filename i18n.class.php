6f52632cc72980fe7c4cb8191dff0bcc9dbd7987
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2014-03-22 13:29:34 +0900

    [i18n] Add: Cache feature.

--- i18n.class.php
+++ i18n.class.php
@@ -9,19 +9,27 @@ class i18n extends Api
 		if(!$to){
 			$to = $this->GetEnv('lang');
 		}
-
+		
 		$url = self::_API_UQUNIE_COM_;
 		$url .= '?';
 		$url .= 'text='.urlencode($text);
 		$url .= '&from='.urlencode($from);
 		$url .= '&to='.urlencode($to);
 		
-		if(!$json = parent::Curl($url)){
-			return $text;
+		//	Cache key.
+		$key = md5($url);
+		
+		//	Check memcache
+		if(!$json = $this->Cache()->Get($key) ){
+			if(!$json = parent::Curl($url)){
+				return $text;
+			}
+			//	Save memcache
+			$this->Cache()->Set( $key, $json );
 		}
 		
+		//	Finish
 		$json = json_decode($json,true);
 		return $json['translate'];
 	}
 }
-
