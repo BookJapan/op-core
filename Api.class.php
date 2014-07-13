c0bc86808c52b58bad9ec2dcde41656279da81fb
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2014-07-13 19:52:03 +0900

    [Api] Chg: Support of cache by each url.

--- Api.class.php
+++ Api.class.php
@@ -5,8 +5,24 @@ class Api extends OnePiece5
 	private $_cookie = null;
 	private $_timeout = 10;
 	
-	function Curl( $url )
+	/**
+	 * Curl
+	 * 
+	 * @param  string  $url
+	 * @param  integer $expire specify of second. 0 is permanently.
+	 * @return string|boolean
+	 */
+	function Curl( $url, $expire=-1 )
 	{
+		//	check cache
+		if( $expire >= 0 ){
+			$ckey = md5($url);
+			if( $body = $this->Cache()->Get($ckey) ){
+				return $body;
+			}
+		}
+		
+		//	init
 		$ch = curl_init();
 		curl_setopt( $ch, CURLOPT_URL, $url );
 		curl_setopt( $ch, CURLOPT_HEADER, true );
@@ -14,16 +30,22 @@ class Api extends OnePiece5
 		curl_setopt( $ch, CURLOPT_COOKIE, $this->_cookie );
 		curl_setopt( $ch, CURLOPT_TIMEOUT, $this->_timeout );
 		
+		//	fail
 		if(!$result = curl_exec($ch)){
 			return false;
 		}
 		
-		//	
+		//	Separate header
 		list($header,$body) = explode("\r\n\r\n",$result);
 		
-		//	Cookieの自動更新
+		//	Cookie
 		if( preg_match('|Set-Cookie: ([^;]+)|', $header, $match) ){
 			$this->_cookie = $match[1];
+		}
+		
+		//	
+		if( $expire >= 0 ){
+			$this->Cache()->Set($ckey, $body, $expire);
 		}
 		
 		return $body;
