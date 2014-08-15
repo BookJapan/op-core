658b2169699bae212436d5bb29dfbad2dfb598f3
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2014-08-16 03:55:11 +0900

    [Model/Camel] Fix: Snake case

--- Model/Camel.model.php
+++ Model/Camel.model.php
@@ -14,22 +14,34 @@ if (!function_exists('lcfirst')) {
 
 class Model_Camel
 {
-	function ConvertSnakeCase($str)
+	/**
+	 * 
+	 * @param  string $str
+	 * @return string
+	 */
+	static function ConvertSnakeCase($str)
 	{
 		$str = trim($str);
-		if( preg_match('/[^-_a-z0-9]/i',$str) ){
-			$this->StackError('Illigal character code.');
-			return;
+		if( preg_match('/[^-_a-z0-9\s]/i',$str) ){
+			OnePiece5::StackError('Illigal character code.');
+			return $str;
 		}
+		//	Abc DEf-G -> abc DEf-G
+		$str = lcfirst($str);
+		//	abc DEf-G -> abc  D Ef- G
 		$str = preg_replace( '/([A-Z])/', ' \\1', $str );
-		$str = preg_replace( '/\s+/', ' ', $str );
+		//	abc  D Ef-G -> abc  D Ef  G
 		$str = str_replace('-', ' ', $str);
+		//	abc  D Ef  G -> abc D Ef G
+		$str = preg_replace( '/\s+/', ' ', $str );
+		//	abc D Ef G -> abc_D_Ef_G
 		$str = str_replace(' ', '_', $str);
+		//	abc_D_Ef_G -> abc_d_ef_g
 		$str = strtolower($str);
 		return $str;
 	}
 	
-	function ConvertPascalCase($str)
+	static function ConvertPascalCase($str)
 	{
 		$str = self::ConvertSnakeCase($str);
 		$str = str_replace('_',' ',$str);
@@ -38,7 +50,7 @@ class Model_Camel
 		return $str;
 	}
 	
-	function ConvertCamelCase($str)
+	static function ConvertCamelCase($str)
 	{
 		$str = self::ConvertPascalCase($str);
 		$str = lcfirst($str);
