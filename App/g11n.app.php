27d29dfb7945b6d2abad511dcbf379defcd7fb6f
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2015-01-05 13:10:22 +0900

    New, App/g11n - Romance dawn.

---
+++ App/g11n.app.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * g11n.app.php
+ * 
+ * Globalization Application model.
+ * 
+ * @package op-core
+ * @since   PHP 5.0
+ * @version 1.0
+ * @author Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ */
+/**
+ * App_g11n
+ * 
+ * Globalization Application model.
+ * 
+ * @package op-core
+ * @since   PHP 5.0
+ * @version 1.0
+ * @author Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ */
+class App_g11n extends App
+{
+	
+}
