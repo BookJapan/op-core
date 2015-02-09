92450b1f7a5fbbaa9c6e7dcd4028d9e2a2fde8a3
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2015-02-10 08:37:28 +0900

    Chg: Model/Model - Added PDO connection logic.

--- Model/Model.model.php
+++ Model/Model.model.php
@@ -22,7 +22,26 @@
  */
 abstract class Model_Model extends OnePiece5
 {
-	
+	function pdo()
+	{
+		static $pdo;
+		if(!$pdo){
+			$pdo = parent::PDO();	
+			if(!$pdo->isConnect()){
+				
+				//  get database config
+				$config = $this->config()->database();
+				$config->password = ';';
+				$this->d($config);
+				
+				//  database connection
+				if(!$io = $pdo->Connect($config)){
+					$this->mark();
+				}
+			}
+		}
+		return $pdo;
+	}
 }
 
 /**
@@ -37,5 +56,30 @@ abstract class Model_Model extends OnePiece5
  */
 abstract class Config_Model extends OnePiece5
 {
+	function _select()
+	{
+		$config = new Config();
+		$config->where->deleted = null;
+		$config->limit = 1;
+		$config->cache = 1;
+		return $config;
+	}
 	
+	function _insert()
+	{
+		$config = new Config();
+		return $config;
+	}
+	
+	function _update()
+	{
+		$config = new Config();
+		return $config;
+	}
+	
+	function _delete()
+	{
+		$config = new Config();
+		return $config;
+	}
 }
