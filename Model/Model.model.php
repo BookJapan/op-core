a92a1b12c40c6c1b3275db8db8fe4a23ade11040
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2015-02-09 10:42:05 +0900

    New: Model/Model - Re born.

---
+++ Model/Model.model.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Model.model.php
+ * 
+ * Creation: 2015-02-09
+ * 
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright 2015 (C) Tomoaki Nagahara All right reserved.
+ */
+
+/**
+ * Model_Model
+ * 
+ * Creation: 2015-02-09
+ * 
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright 2015 (C) Tomoaki Nagahara All right reserved.
+ */
+abstract class Model_Model extends OnePiece5
+{
+	
+}
+
+/**
+ * Config_Model
+ * 
+ * Creation: 2015-02-09
+ * 
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright 2015 (C) Tomoaki Nagahara All right reserved.
+ */
+abstract class Config_Model extends OnePiece5
+{
+	
+}
